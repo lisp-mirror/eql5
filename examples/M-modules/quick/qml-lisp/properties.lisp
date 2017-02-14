c90c74c93122f18da17d9afef73c3034c2546f4d
polos <polos.ruetz@gmail.com>
2017-02-14 12:35:51 +0100

    small revision of property dialog for QML

--- examples/M-modules/quick/qml-lisp/properties.lisp
+++ examples/M-modules/quick/qml-lisp/properties.lisp
@@ -4,8 +4,11 @@
   (find-symbol (symbol-name name) package))
 
 (defun show-properties-dialog (&optional item)
+  "Lists all instance properties of a QML item (either a QQuickItem or an 'objectName'). If no item is passed, QML:*CALLER* will be used."
   (unless (find-package :properties)
     (load (in-home "gui/properties")))
+  (when (stringp item)
+    (setf item (qml:find-quick-item item)))
   (funcall (sym :show :properties)
            (or item qml:*caller*)
            t)) ; all instance properties (for QML)
