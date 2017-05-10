754cc8ee39e4b1eef1ec9ed8601e9cf31badea7e
polos <polos.ruetz@gmail.com>
2017-05-10 16:50:53 +0200

    add macro SYNCALL to WebEngine example (for synchronous funcalls)

--- examples/M-modules/webengine/minimal.lisp
+++ examples/M-modules/webengine/minimal.lisp
@@ -9,6 +9,7 @@
 
 ;; N.B: The following functions are asynchronous (because WebEngine
 ;; is all asynchronous).
+;;
 ;; So, we won't have a return value, but need to pass a function.
 ;; (See 'FunctorOrLambda' in both Qt Assistant and EQL sources.)
 
@@ -20,6 +21,27 @@
   ;; example: (js "document.title")
   (|runJavaScript| (|page| *view*) expression
                    (lambda (result) (qmsg result))))
+
+;; Introducing macro SYNCALL allows to call asynchronous functions
+;; as if they were synchronous (more convenient in simple cases).
+
+(defmacro syncall (expression)
+  "Converts asynchronous function calls to synchronous ones, using a local event loop."
+  (let ((result (gensym))
+        (ev-loop (gensym)))
+    `(let (,result)
+      (qlet ((,ev-loop "QEventLoop"))
+        (,@expression (lambda (x)
+                        (setf ,result x)
+                        (|exit| ,ev-loop)))
+        (|exec| ,ev-loop))
+      ,result)))
+
+(defun to-text* ()
+  (print (syncall (|toPlainText| (|page| *view*)))))
+
+(defun js* (expression)
+  (qmsg (syncall (|runJavaScript| (|page| *view*) expression))))
 
 (x:do-with *view*
   (|setUrl| (qnew "QUrl(QString)" "http://planet.lisp.org/"))
